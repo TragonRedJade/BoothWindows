e31976c7b47411770e4d6d68284787633cdf5df0
elemke@tragon.com <elemke@tragon.com>
2014-11-18 11:28:04 -0600

    windows xp support

--- app/js/main.js
+++ app/js/main.js
@@ -25,37 +25,44 @@ var fs = require('fs');
 var path = require('path');
 var os = require("os");
 
-if (os.platform() == "win32") {
-    var root_drive = (os.platform == "win32") ? process.cwd().split(path.sep)[0] : "/";
-    var key_file_path = root_drive + "ProgramData/RedJade/appkey.txt";
-} 
-else {
-    var key_file_path = path.dirname(process.execPath).replace(/\\/g, "/") + "/appkey.txt";    
+window.onload = function () {
+    
+    if (os.platform() == "win32") {
+        var root_drive = (os.platform == "win32") ? process.cwd().split(path.sep)[0] : "/";
+        try {
+            fs.statSync(root_drive + "ProgramData");
+            try {
+                fs.statSync(root_drive + "ProgramData/RedJade");
+            } catch (err) {
+                //handleError(err);
+                fs.mkdirSync(root_drive + 'ProgramData/RedJade', 0755);
+            }
+            var key_file_path = root_drive + "ProgramData/RedJade/appkey.txt";
+        } catch(err) {
+            
+        }
+    }
+    
+    if (!key_file_path) {
+        var key_file_path = path.dirname(process.execPath).replace(/\\/g, "/") + "/appkey.txt";
+    }
+
+    fs.readFile(key_file_path, 'utf-8', function (error, contents) {
+	    if(!contents){
+		    console.log("no contents");
+		    var new_guid = guid();
+		    fs.writeFile(key_file_path, new_guid, function (err) {
+			    if(err) {
+			        console.log(err);
+			        handleError(err);
+			    } else {
+					    console.log("success on write");
+			    }
+		    });
+		    updateIFrameSrc(new_guid);
+	    }else{
+		    console.log(contents);
+		    updateIFrameSrc(contents);
+	    }
+    });
 }
-
-
-
-//window.onload=function() {
-//    handleError(os.platform());
-//    handleError(key_file_path);
-//}
-
-fs.readFile(key_file_path, 'utf-8', function (error, contents) {
-	if(!contents){
-		console.log("no contents");
-		var new_guid = guid();
-		fs.writeFile(key_file_path, new_guid, function (err) {
-			if(err) {
-			    console.log(err);
-			    handleError(err);
-			} else {
-					console.log("success on write");
-			}
-		});
-		updateIFrameSrc(new_guid);
-	}else{
-		console.log(contents);
-		updateIFrameSrc(contents);
-	}
-});
-
